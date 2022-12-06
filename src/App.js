399ff8141a3aaa0a44b4038d028a7390124093a6
htkachuk <vladyslav.radetskyi@gmail.com>
2022-12-06 23:45:57 +0200

    add TopBar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import TopBar from "./componets/TopBar";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <TopBar />
+    </div>
+  );
 }
 
 export default App;
