15f2bac08c3272d94230c7f8aab5d3ba4e2ea99e
htkachuk <vladyslav.radetskyi@gmail.com>
2022-12-07 18:09:09 +0200

    add NavBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import NavBar from "./componets/NavBar";
 import TopBar from "./componets/TopBar";
 
 function App() {
   return (
     <div>
       <TopBar />
+      <NavBar />
     </div>
   );
 }
